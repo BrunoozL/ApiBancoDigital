f9b0b365ffdbcf555bbf04e47e3e4acbc8e93393
Bruno Righi <brunorighi267@gmail.com>
2023-04-17 09:05:34 -0300

    update

---
+++ rotas.php
@@ -0,0 +1,29 @@
+<?php
+
+    use App\Controller\;
+
+    $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    switch ($url)
+    {
+        case '/correntista/save':
+        break;
+
+        case '/conta/extrato':
+        break;
+
+        case '/conta/pix/enviar':
+        break;
+
+        case '/conta/pix/receber':
+        break;
+
+        case '/correntista/entrar':
+        break;
+
+        default:
+            http_response_code(403);
+        break;
+    }
+
+?>
